c3ca63ed08e2c90714b2d7884b2b8ef9ac2b0b14
sun4205 <hanna4205@gmail.com>
2024-12-06 21:12:47 -0500

    server request practice

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 console.log("hi");
 console.log(3+5)
 
-const http = require("http");
-const server = http.createServer((req,res)=>{
-    res.writeHead(200,{
-        'Content-Type' : 'text/html; charset=utf8'
-    });
-    res.end('<h1>Hello, World','utf-8');
-});
-server.listen(3000);
+// const http = require("http");
+// const {PORT=3000} = process.env;
+
+// const server = http.createServer((req,res)=>{
+//     res.writeHead(200,{
+//         'Content-Type' : 'text/html; charset=utf8'
+//     });
+//     res.end('<h1>Hello, World','utf-8');
+// });
+// server.listen(PORT);
+
+// const http = require('http');
+// const server = http.createServer((req,res)=>{
+//     const data = '';
+//     req.on('data',(chunk)=>{
+//         data += chunk.toString();
+//     });
+//     req.on('end',()=>{
+//         console.log(JSON.parse(data));
+//     })
+// })
+
+// server.listen(3000);
+
+const server =http.createServer((req,res)=>{
+    if(req.url ==='/songs' && req.method ==='GET'){
+        console.log('get the list of the songs');
+    }
+})
