d8a2d85f91906dd732ee182647692d4158b99d3d
sun4205 <hanna4205@gmail.com>
2024-12-12 17:43:48 -0500

    server practice

--- index.js
+++ index.js
@@ -1,153 +1,40 @@
-console.log("hi");
-console.log(3+5)
-
-// const http = require("http");
-// const {PORT=3000} = process.env;
-
+// const http = require('http');
+// const server = http.createServer((req,res)=>{
+//     console.log(req.url);
+//     console.log(req.method);
+//     console.log(req.headers);
+//     console.log(req.body);
+//     res.statusCode =200;
+//     res.statusMessage = 'OK';
+//     res.setHeader('Content-Type','text/plain');
+//     res.write('Hello');
+//     res.write('World');
+//     res.end();
+// })
+// server.listen(3000);
 // const server = http.createServer((req,res)=>{
 //     res.writeHead(200,{
-//         'Content-Type' : 'text/html; charset=utf8'
+//         'Content-Type':'text/html; charset=utf8'
 //     });
-//     res.end('<h1>Hello, World','utf-8');
-// });
-// server.listen(PORT);
+//     res.end('<h1>Hello World</h1>','utf-8');
+// })
 
 // const http = require('http');
 // const server = http.createServer((req,res)=>{
-//     const data = '';
-//     req.on('data',(chunk)=>{
+//     let data = '';
+//     req.on('data', (chunk)=>{
 //         data += chunk.toString();
 //     });
+
 //     req.on('end',()=>{
 //         console.log(JSON.parse(data));
-//     })
-// })
+//     });
+// });
 
 // server.listen(3000);
 
-const http = require('http');
-
-const { PORT = 3000, BASE_PATH } = process.env;
-
-const mainPageMarkup = `
-  <!DOCTYPE html>
-  <html>
-  <head>
-    <meta charset="utf-8">
-    <title>To-do list</title>
-    <style>
-      html, body {
-        font-family: Helvetica, Arial, sans-serif;
-        -webkit-font-smoothing: antialiased;
-        height: 100%;
-        width: 100%;
-        display: flex;
-        margin: 0;
-      }
-
-      input, button {
-        border: none;
-      }
-
-      .container {
-        width: 468px;
-        margin: 0 auto;
-        padding-top: 100px;
-      }
-
-      h1 {
-        font-weight: bold;
-      }
-
-      .input {
-        display: flex;
-        justify-content: space-between;
-      }
-
-      .input__text {
-        font-size: 0.8em;
-        width: 310px;
-        height: 50px;
-        border-bottom: 1px solid #f1f1f1;
-        padding: 0 10px;
-        box-sizing: border-box;
-      }
-
-      .input__elem_text::placeholder {
-        color: #d3d3d3;
-      }
-
-      .input__btn {
-        font-size: 0.8em;
-        width: 150px;
-        height: 50px;
-        background-color: #ffdb4d;
-        border-radius: 2px;
-        cursor: pointer;
-      }
-    </style>
-  </head>
-  <body>
-    <form class="container" action="${BASE_PATH}/submit" method="POST" enctype="text/plain">
-      <h1>To-do list</h1>
-      <div class="input">
-        <input type="text" placeholder="New task" class="input__text" name="item">
-        <button class="input__btn input__btn_add">
-          Add
-        </button>
-      </div>
-    </form>
-  </body>
-  </html>
-`;
-const submitSuccessMarkup = ` <!DOCTYPE html>
-  <html>
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link rel="stylesheet" href="style.css">
-    <title>To-do list</title>
-    <style>
-      html,
-      body {
-        font-family: Helvetica, Arial, sans-serif;
-        -webkit-font-smoothing: antialiased;
-        height: 100%;
-        width: 100%;
-        display: flex;
-        margin: 0;
-      }
-
-      .container {
-        width: 468px;
-        margin: 0 auto;
-        padding-top: 100px;
-      }
-
-      h1 {
-        font-weight: bold;
-      }
-    </style>
-  </head>
-  <body>
-    <div class="container">
-      <h1>Form sent successfully</h1>
-      <a href="${BASE_PATH}">Go back</a>
-    </div>
-  </body>
-  </html>`
-const server =http.createServer((req,res)=>{
-    if(req.url='/submit'&& req.method ==='POST'){
-        res.writeHead(200,{
-            'Content-Type':'text/html'
-        });
-        res.end(submitSuccessMarkup);
-    }
-    if(req.url='/'&& req.method === 'GET'){
-        res.writeHead(200,{
-            'Content-Type':'text/html'
-        });
-        res.end(mainPakageMark)
+const server = http.creatServer((req,res)=>{
+    if(req.url === '/songs' && req.method === 'GET'){
+        console.log('GET the list of songs');
     }
 })
-server.listen(port);
