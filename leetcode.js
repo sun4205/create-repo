dc04833c273a842efe4b6f6dc2c0991b212aa13b
sun4205 <hanna4205@gmail.com>
2024-12-12 17:44:10 -0500

    median two sorted array

--- leetcode.js
+++ leetcode.js
@@ -72,3 +72,18 @@ nums =
 target =
 9
 
+//median of two sorted array.
+var removeDuplicates = function(nums) {
+    if (nums.length === 0) return 0; 
+    
+    let k = 1; 
+    
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] !== nums[i - 1]) { 
+            nums[k] = nums[i];       
+            k++;                     
+        }
+    }
+    
+    return k; 
+};
