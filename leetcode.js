19685babbf4bc1da8ea3ba1724a1c11a8bdf6c07
sun4205 <hanna4205@gmail.com>
2024-12-13 13:48:16 -0500

    remove element

--- leetcode.js
+++ leetcode.js
@@ -87,3 +87,15 @@ var removeDuplicates = function(nums) {
     
     return k; 
 };
+
+//remove element
+var removeElement = function(nums, val) {
+    let k =0;
+    for(let i=0; i<nums.length; i++){
+        if(nums[i] !== val){
+            nums[k] = nums[i];
+            k++;
+        }
+    }
+    return k;
+};
