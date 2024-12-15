cb52eefb7e28eea52bc87c89ff6ee321acc7ae70
sun4205 <hanna4205@gmail.com>
2024-12-14 19:36:11 -0500

    plus one

--- leetcode.js
+++ leetcode.js
@@ -99,3 +99,19 @@ var removeElement = function(nums, val) {
     }
     return k;
 };
+
+//plus one
+var plusOne = function(digits) {
+    let n = digits.length;
+
+    for(let i=n-1; i>=0; i--){
+        if(digits[i]<9){
+            digits[i] += 1;
+            return digits;
+        }
+        digits[i] =0;
+    }
+        digits.unshift(1);
+        return digits;
+    }
+    
