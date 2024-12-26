ac9862c6b4035844651b0de4fa0a3ad7c12998cd
sun4205 <hanna4205@gmail.com>
2024-12-25 22:21:42 -0500

    auth practice

--- review.js
+++ review.js
@@ -1,45 +1,23 @@
-const mongoose = require("mongoose");
-const validator = require("validator");
+const jwt = reqiire("jsonwebtoken");
+const { JWT_SECRET } = require("../utils/config");
+const { UNATHURIZED } = require("../utils/errors");
 
-const clothingSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    minlength: 2,
-    maxlength: 30,
-  },
-  weather: {
-    type: String,
-    required: true,
-    enum: {
-      values: ["hot", "warm", "cold"],
-      message: "Weather must be 'hot', 'warm', or 'cold'.",
-    },
-  },
-  imageUrl: {
-    type: String,
-    require: true,
-    validate: {
-      validator(value) {
-        return validator.isURL(value);
-      },
-      message: "You must enter a valid URL for the image",
-    },
-  },
-  owner: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "user",
-    required: true,
-  },
-  likes: {
-    type: [mongoose.Schema.Types.ObjectId],
-    ref: "user",
-    default: [],
-  },
-  createAt: {
-    type: Date,
-    default: Date.now,
-  },
-});
+module.exports = (req, res, next) => {
+  const { authorization } = req.headers;
 
-module.exports = mongoose.model("Item", clothingSchema);
+  if (!authorization || !authorization.startsWith("Bearer")) {
+    return res.status(UNATHURIZED).send({ message: "Authorization required" });
+  }
+  const token = authorization.replace("Bearer", "");
+
+  let payload;
+
+  try {
+    payload = jwt.verify(token, JWT_SECRET);
+  } catch (err) {
+    return res.status(UNATHURIZED).send({ message: "Invalid token" });
+  }
+  req.user = payload;
+
+  return next();
+};
