8d09a379b564b99a65e723eb3c1f84e8c982be4f
sun4205 <hanna4205@gmail.com>
2025-01-02 13:07:13 -0500

    token review

--- review.js
+++ review.js
@@ -1,45 +1,11 @@
-const mongoose = require("mongoose");
-const Item = require("../models/clothingItem");
-const {
-  BAD_REQUEST,
-  NOT_FOUND,
-  INTERNAL_SERVER_ERROR,
-  FORBIDDEN,
-} = require("../utils/eerors");
+const TOKEN_KEY = "jwt";
 
-const isValidObjectId = (id) => mongoose.Types.ObjectId.isValid(id);
+export const setToken = (token) => localStorage.setItem(TOKEN_KEY, token);
 
-const getItems = (req, res) => {
-  Item.find({})
-    .then((items) => res.send(items))
-    .catch((err) => {
-      console.error(err);
-      return res
-        .status(INTERNAL_SERVER_ERROR)
-        .send({ message: "An error has occurred on the server." });
-    });
+export const getToken = () => {
+  return localStorage.getItem(TOKEN_KEY);
 };
 
-const createItem = (req, res) => {
-  const { name, weather, imageUrl } = req.body;
-
-  if (!req.user || !req.user._id) {
-    return res
-      .status(BAD_REQUEST)
-      .send({ message: "User is not authenticated." });
-  }
-
-  const owner = req.user._id;
-
-  return Item.create({ name, weather, imageUrl, owner })
-    .then((item) => res.status(201).send(item))
-    .catch((err) => {
-      console.error(err);
-      if (err.name === "ValidationError") {
-        return res.status(BAD_REQUEST).send({ message: err.message });
-      }
-      return res
-        .status(INTERNAL_SERVER_ERROR)
-        .send({ message: "An error occurred on the server." });
-    });
+export const removeToken = () => {
+  localStorage.removeItem(TOKEN_KEY);
 };
