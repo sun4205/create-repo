5fcbe22ce4cb37607cb4dee25f6643d55dcb240d
sun4205 <hanna4205@gmail.com>
2025-01-03 14:42:20 -0500

    data practice

--- review.js
+++ review.js
@@ -1,12 +1,20 @@
-export const register = (username, password, email) => {
-  return fetch(`${BASE_URL}/auth/local/register`, {
-    method: "POST",
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
+const data = {
+  ducks: [
+    {
+      id: 0,
+      name: "Dog Duck",
+      description:
+        'This rare duck has been spotted on a number of occasions throughout central Europe. It is said to enjoy playing with old socks that it finds in the water. Tanya Peterson was 25 when she saw this duck, she reported, "It was quite a friendly little fellow. At no time did I feel any fear. I stayed with him for maybe 15 or 20 minutes, then zoom! He zipped out of the water and across the night sky like a comet.',
+      img: new URL("../assets/images/dog-duck.png", import.meta.url).href,
     },
-    body: JSON.stringify({ username, password, email }),
-  }).then((res) => {
-    return res.ok ? res.json() : Promise.reject(`Error: ${res.status}`);
-  });
+    {
+      id: 1,
+      name: "Zebra Duck",
+      description:
+        "This skittish beauty has been sighted in central Africa. Cryptoduckologists assert that the best time to see one of these magnificent quackers is when the full moon is at its highest point on a windless night.",
+      img: new URL("../assets/images/zebra-duck.png", import.meta.url).href,
+    },
+  ],
 };
+
+export default data;
