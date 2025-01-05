ee9c40c23687caf5de8eea71050cb549f89a1c70
sun4205 <hanna4205@gmail.com>
2025-01-04 22:41:21 -0500

    review

--- review.js
+++ review.js
@@ -1,11 +1,5 @@
-const TOKEN_KEY = "jwt";
+const [userData, setUserData] = useState({ username: "", email: "" });
+const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-export const setToken = (token) => localStorage.setItem(TOKEN_KEY, token);
-
-export const getToken = () => {
-  return localStorage.getItem(TOKEN_KEY);
-};
-
-export const removeToken = () => {
-  localStorage.removeItem(TOKEN_KEY);
-};
+const navigate = useNavigate();
+const location = useLocation();
