2b74204ca82a84c9d110d42dabec2867528a86ea
sun4205 <hanna4205@gmail.com>
2025-02-04 20:29:04 -0500

    removeDuplicates

--- leetcode1.js
+++ leetcode1.js
@@ -12,3 +12,14 @@ const twoSum = function (nums, target) {
   }
 };
 
+const removeDuplicates = function (nums){
+    let k=1;
+    for(let i=0; i<nums.length-1; i++){
+        if(nums[i] !== nums[i+1]){
+            nums[k] = nums[i+1];
+            k++;
+        }
+    }
+    return k;
+}
+
