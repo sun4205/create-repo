62bae566b911942b6228761e8c141d55caf73be5
sun4205 <hanna4205@gmail.com>
2025-02-08 18:57:13 -0500

    plusone

--- leetcode1.js
+++ leetcode1.js
@@ -50,3 +50,15 @@ const searchInsert = function (nums, target) {
 
   return left;
 };
+
+const plusOne = function(digits) {
+    for (let i = digits.length - 1; i >= 0; i--) {
+        if (digits[i] < 9) {
+            digits[i]++;  
+            return digits;
+        }
+        digits[i] = 0;  
+    }
+    digits.unshift(1);  
+    return digits;
+};
