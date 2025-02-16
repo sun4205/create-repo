7d25e38df030898b93daf192f9c6f5716881616f
sun4205 <hanna4205@gmail.com>
2025-02-16 18:28:42 -0500

    practice

--- leetcode1.js
+++ leetcode1.js
@@ -25,6 +25,17 @@ var twoSum = function(nums, target) {
 return [];
 };
 
+
+var twoSum = function(nums, target) {
+  for(let i=0; i<nums.length; i++) {
+      for(let j=i+1; j<nums.length; j++) {
+          if(nums[i]+nums[j] === target) {
+              return [i, j]
+          }
+      }
+  }
+};
+
 const removeDuplicates = function (nums) {
   let k = 1;
   for (let i = 0; i < nums.length - 1; i++) {
