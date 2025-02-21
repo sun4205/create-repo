99a62bc2c716fcca65739366ef7d2874dadcc097
sun4205 <hanna4205@gmail.com>
2025-02-20 21:22:47 -0500

    format

--- leetcode1.js
+++ leetcode1.js
@@ -148,14 +148,13 @@ function singleNumber(nums) {
 function singleNumber(nums) {
   const count = {};
 
- 
   for (let num of nums) {
     count[num] = (count[num] || 0) + 1;
   }
 
   for (let num in count) {
     if (count[num] === 1) {
-      return Number(num); 
+      return Number(num);
     }
   }
 }
