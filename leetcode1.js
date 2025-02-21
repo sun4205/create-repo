331f4b350e860699853a82d7a69eba3bd3375ff9
sun4205 <hanna4205@gmail.com>
2025-02-20 21:22:27 -0500

    add single number

--- leetcode1.js
+++ leetcode1.js
@@ -144,3 +144,18 @@ function singleNumber(nums) {
   }
   return result;
 }
+
+function singleNumber(nums) {
+  const count = {};
+
+ 
+  for (let num of nums) {
+    count[num] = (count[num] || 0) + 1;
+  }
+
+  for (let num in count) {
+    if (count[num] === 1) {
+      return Number(num); 
+    }
+  }
+}
