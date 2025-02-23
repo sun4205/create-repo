665ac5756cc793b0644df0dc6dfe26058916f154
sun4205 <hanna4205@gmail.com>
2025-02-22 19:27:31 -0500

    practice api

--- practice.js
+++ practice.js
@@ -1,17 +1,8 @@
-function getCardElement(data) {
-  const cardElement = cardTemplate.content
-    .querySelector(".card")
-    .cloneNode(true);
-  const cardNameEl = cardElement.querySelector(".card__title");
-  const cardImageEl = cardElement.querySelector(".card__image");
-  const cardLikeBtn = cardElement.querySelector(".card__like-btn");
-  const cardDeleteBtn = cardElement.querySelector(".card__delete-btn");
+const api = new Api({
+  baseUrl:"https://around-api.en.tripleten-services.com/v1",
+  headers: {
+    authorization:"",
+    "Content-type":"application/json",
+  },
+});
 
-  cardNameEl.textContent = data.name;
-  cardImageEl.src = data.link;
-  cardImageEl.alt = data.name;
-
-  if (data.isLiked) {
-    cardLikeBtn.classList.add("card__like-btn_liked");
-  }
-}
