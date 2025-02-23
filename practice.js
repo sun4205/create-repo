fe9e27f8acb80997b6e766a4a2b9c61170fe9f90
sun4205 <hanna4205@gmail.com>
2025-02-22 20:25:13 -0500

    api practice

--- practice.js
+++ practice.js
@@ -1,8 +1,27 @@
-const api = new Api({
-  baseUrl:"https://around-api.en.tripleten-services.com/v1",
-  headers: {
-    authorization:"",
-    "Content-type":"application/json",
-  },
-});
+class Api {
+  constructor({ baseUrl, headers }) {
+    this._baseUrl = baseUrl;
+    this._headers = headers;
+  }
+ 
 
+  _checkResponse(res) {
+    if (res.ok) {
+      return res.json();
+    }
+    return Promise.reject(`Error: ${res.status}`);
+  }
+
+  
+
+  _request(endpoint, options = {}) {
+    const finalOptions = {
+      headers: this._headers,
+      ...options,
+    };
+    const url = `${this._baseUrl}${endpoint}`;
+    return fetch(url, finalOptions).then(this._checkResponse);
+  }
+ 
+
+}
