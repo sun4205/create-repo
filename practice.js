16c5ff404aa43230d78592fb7ceef5d5cb80507c
sun4205 <hanna4205@gmail.com>
2025-02-24 21:34:11 -0500

    api practice

--- practice.js
+++ practice.js
@@ -10,4 +10,33 @@ function getItems() {
   return request(`${baseUrl}/items`);
 }
 
+const addItem = ({ name, weather, imageUrl }) => {
+  return request(`${baseUrl}/items`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      authorization: `Bearer ${localStorage.getItem("jwt")}`,
+    },
+    body: JSON.stringify({
+      name,
+      weather,
+      imageUrl,
+    }),
+  });
+};
+
+const removeItem = (_id) => {
+  const token = localStorage.getItem("jwt");
+  console.log("Deleting item with _id:", _id);
+  return request(`${baseUrl}/items/${_id}`, {
+    method: "DELETE",
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "application/json",
+    },
+  });
+};
+
+
+
 
