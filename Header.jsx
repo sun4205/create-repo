adec8afc24e99f9fe4dc0ec85c46bd313eded850
sun4205 <hanna4205@gmail.com>
2025-02-25 19:11:47 -0500

    review project

---
+++ Header.jsx
@@ -0,0 +1,14 @@
+function Header() {
+    return 
+    <header className="header">
+        <img src="" alt="" className="header__logo"/>
+        <p className="header__date-and-location">date, location</p>
+        <button className="header__add-clothes-btn">+add Clothes </button>
+        <div className="header__user-container">
+            <p className="header__username">name</p>
+            <img src="" alt="" className="header__userAvatar"/>
+        </div>
+    </header>
+}
+
+export default Header;
---
+++ practice.jsx
@@ -0,0 +1,15 @@
+import Header from "./Header"
+
+function App () {
+    return (
+        <div className="page">
+            <div className="page-content">
+                <Header />
+            </div>
+
+        </div>
+    )
+
+}
+
+export default App;
