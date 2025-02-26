2ea53d7f3ec99c4f2fb9adc66d0af699e6cbae27
sun4205 <hanna4205@gmail.com>
2025-02-25 21:57:21 -0500

    review project

--- ModalWithForm.jsx
+++ ModalWithForm.jsx
@@ -1,19 +1,12 @@
-function ModalWithForm(){
+function ModalWithForm({title,buttonText}){
     return(
         <div className="modal">
             <div className="modal-content">                
-                    <p className="modal__title">New Garment</p>
+                    <p className="modal__title">{title}</p>
                     <button type="button" className="modal__close-btn">close</button>
                     <form className="modal__form">
-                    <label htmlFor="name" className="modal__label">Name <input id="name" type="text" className="modal__input" placeholder="name"/></label>
-                    <label htmlFor="imageUrl" className="modal__label">ImageUrl <input id="imageUrl" type="text" className="modal__input" placeholder="imageUrl"/></label>
-                    <fieldset className="modal__radio-buttons">
-                        <legend className="modal__legent">select the weather type</legend>
-                        <label htmlFor="hot" className="modal__label modal__label_type-radio">hot <input type="radio" id="hot" /></label>
-                        <label htmlFor="warm" className="modal__label modal__label_type-radio">warm <input type="radio" id="warm" /></label>
-                        <label htmlFor="cold" className="modal__label modal__label_type-radio">cold <input type="radio" id="cold" /></label>
-                    </fieldset>
-                    <button type="submit" className="modal__submit">submit</button>
+                    
+                    <button type="submit" className="modal__submit">{buttonText}</button>
                 </form>
             </div>
         </div>
--- practice.jsx
+++ practice.jsx
@@ -1,11 +1,24 @@
 import Header from "./Header"
+import Main from "./Main"
+import ModalWithForm from "./ModalWithForm";
 
 function App () {
     return (
         <div className="page">
             <div className="page-content">
                 <Header />
+                <Main weatherData ={weatherData} />
             </div>
+            <ModalWithForm title="New garment" buttonText="add garment">
+            <label htmlFor="name" className="modal__label">Name <input id="name" type="text" className="modal__input" placeholder="name"/></label>
+                    <label htmlFor="imageUrl" className="modal__label">ImageUrl <input id="imageUrl" type="text" className="modal__input" placeholder="imageUrl"/></label>
+                    <fieldset className="modal__radio-buttons">
+                        <legend className="modal__legent">select the weather type</legend>
+                        <label htmlFor="hot" className="modal__label modal__label_type-radio">hot <input type="radio" id="hot" /></label>
+                        <label htmlFor="warm" className="modal__label modal__label_type-radio">warm <input type="radio" id="warm" /></label>
+                        <label htmlFor="cold" className="modal__label modal__label_type-radio">cold <input type="radio" id="cold" /></label>
+                    </fieldset>
+            </ModalWithForm>
 
         </div>
     )
