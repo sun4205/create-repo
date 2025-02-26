1c34322d646d944819aa8fa581be3e834e87cd12
sun4205 <hanna4205@gmail.com>
2025-02-25 19:12:25 -0500

    format

--- Header.jsx
+++ Header.jsx
@@ -1,14 +1,15 @@
 function Header() {
-    return 
+  return (
     <header className="header">
-        <img src="" alt="" className="header__logo"/>
-        <p className="header__date-and-location">date, location</p>
-        <button className="header__add-clothes-btn">+add Clothes </button>
-        <div className="header__user-container">
-            <p className="header__username">name</p>
-            <img src="" alt="" className="header__userAvatar"/>
-        </div>
+      <img src="" alt="" className="header__logo" />
+      <p className="header__date-and-location">date, location</p>
+      <button className="header__add-clothes-btn">+add Clothes </button>
+      <div className="header__user-container">
+        <p className="header__username">name</p>
+        <img src="" alt="" className="header__userAvatar" />
+      </div>
     </header>
+  );
 }
 
 export default Header;
