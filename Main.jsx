e098592ba0641c8c2e8e6b6beb7e339728d66c59
sun4205 <hanna4205@gmail.com>
2025-02-25 19:36:20 -0500

    review project

---
+++ Main.jsx
@@ -0,0 +1,15 @@
+import WeatherCard from "./WeatherCard";
+
+function Main() {
+    return(
+        <main>
+            <WeatherCard />        
+        <section className="cards">
+            <p className="cards__text">You may want to wear</p>
+            <Cards />
+        </section>
+        </main>
+    )
+}
+
+export default Main;
---
+++ WeatherCard.jsx
@@ -0,0 +1,11 @@
+function WeatherCard() {
+    return(
+        <section className="weather-card">
+            <p className="weather-card__temp"></p>
+            <img src="" alt="" className="weather-card__img" />
+
+        </section>
+    )
+}
+
+export default WeatherCard;
