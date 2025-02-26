7d881bff6a75c7453ded287fe3b6baae8e9adf7a
sun4205 <hanna4205@gmail.com>
2025-02-26 14:34:37 -0500

    review project

--- Header.jsx
+++ Header.jsx
@@ -1,8 +1,9 @@
 function Header({handleAddClick}) {
+  const currentDate = new Date().toLocaleString('default', { month: 'long', day: 'numeric' });
   return (
     <header className="header">
       <img src="" alt="" className="header__logo" />
-      <p className="header__date-and-location">date, location</p>
+      <p className="header__date-and-location">{currentDate}, location</p>
       <button onClick={handleAddClick} type="button" className="header__add-clothes-btn">+add Clothes </button>
       <div className="header__user-container">
         <p className="header__username">name</p>
