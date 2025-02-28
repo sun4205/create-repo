8a66be8a4ebdba79199d2942ab4d503383897df7
sun4205 <hanna4205@gmail.com>
2025-02-27 21:38:46 -0500

    reveiw project

--- review.js
+++ review.js
@@ -1,4 +1,9 @@
-function checkResponse(res) {
-  return res.ok ? res.json() : Promise.reject(`Error: ${res.status}`);
-}
-
+const handleAddItemSubmit = (item) => {
+  console.log("handleAddItemSubmit called with item:", item);
+  asyncSubmit(() =>
+    addItem(item).then((newItem) => {
+      console.log("Current clothingItems before update:", clothingItems);
+      setClothingItems([newItem, ...clothingItems]);
+    })
+  );
+};
