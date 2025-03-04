f427548fdfb8b2482019eb11c8805508d32e7ab4
sun4205 <hanna4205@gmail.com>
2025-03-03 20:44:00 -0500

    practice project

--- review.js
+++ review.js
@@ -1,162 +1,31 @@
-const handleAddClick = () =>{
-  setActiveModal("add-garment");
-}
+export const weatherOptions = [
+  {
+    day: true,
+    condition: "sunny",
+    url: new URL("../assets/day/sunny.svg", import.meta.url).href,
+  },
+  {
+    day: true,
+    condition: "clouds",
+    url: new URL("../assets/day/cloudy.svg", import.meta.url).href,
+  },
+]
 
-const changeCurrentUserData =(username, avatar) =>{
-  setActiveModal("edit-profile");
-}
-
-const openRegisterModal = () => {
-  setActiveModal("register");
-}
-
-const openLoginModal = () => {
-  setActiveModal("login");
-}
-
-const closeActiveModal = () => {
-  setActiveModal("");
-}
-
-const handleToggleSwitchChange = () => {
-  setCurrentTemperatureUnit((prevUnit)=>(prevUnit==="F"?"C":"F"));
-}
-
-function asyncSubmit(request) {
-  setIsLoading(true);
-  request()
-    .then(closeActiveModal)
-
-    .catch(console.error)
-
-    .finally(() => setIsLoading(false));
-}
-const handleAddItemSubmit = (item) => {
-  console.log("handleAddItemSubmit called with item:", item);
-  asyncSubmit(() =>
-    addItem(item).then((newItem) => {
-      console.log("Current clothingItems before update:", clothingItems);
-      setClothingItems([newItem, ...clothingItems]);
-    })
-  );
-};
-
-function getUserInformation(token) {
-  return auth
-    .getUserInfo(token)
-    .then((user) => {
-      setIsLoggedIn(true);
-      setCurrentUser(user);
-    })
-    .catch(console.error);
-}
-
-const handleLogin = ({ username, password }) => {
-  if (!username || !password) {
-    return;
-  }
-
-  auth
-    .authorize(username, password)
-    .then((data) => {
-      if (data.token) {
-        setToken(data.token);
-        setIsLoggedIn(true);
-        getUserInformation(data.token).then(() => {
-          const redirectPath = location.state?.from?.pathname || "/";
-          navigate(redirectPath);
-          closeActiveModal();
-        });
-      }
-    })
-    .catch(console.error);
-};
-
-
-useEffect = (()=>{
-  const jwt = getToken();
-  if(!jwt){
-    setCurrentUser(null);
-    setIsLoggedIn(false);
-    return;
-  }
-
-  getUserInformation(jwt);
-},[]);
-
-const handleLogOut = () => {
-  console.log("Log Out button clicked.");
-  removeToken();
-  setIsLoggedIn(false);
-  setCurrentUser(null);
-  navigate("/");
-  console.log("User logged out successfully.");
-};
-
-const updatedUserSubmit = ({
-  username,
-  avatar,
-  setCurrentUser,
-  closeActiveModal
-}) => {
-  updatdeUserData(username,avatar,setCurrentUser,closeActiveModal)
-  .then((updatedUser)=>{
-    setCurrentUser(updatedUser);
-    closeActiveModal();
-  })
-  .catch((error)=>console.error)
-}
-
-const handleCardLike = ({id,isLiked}) => {
-  const token = localStorage.getItem("jwt");
-  !isLiked
-  ?api
-  .addCardLike(id,token)
-  .then((updatedCard)=>{
-    setClothingItems((cards)=>cards.map((card)=>card._id===id?updatedCard:card))
-  })
-  .catch((err)=>console.log(err))
-  : api
-  .removeCardLike(id,token)
-  .then((updatedCard)=>{
-    setClothingItems((cards)=>cards.map((card)=>card._id===id?updatedCard:card))
-  })
-  .catch((err)=>console.log(err))
-}
-
-const handleRegisterSubmit = (values) => {
-  asyncSubmit(()=>{
-    auth.register(values.name,values.avatar,values.email,values.password)
-    .then(()=>{
-      handleLogin({username:values.email,password:values.password});
-    })
-  })
-}
-
-const handleDeleteConfirm = () => {
-  if(selectedCard){
-    asyncSubmit(()=>{
-      removeItem(selectedCard._id)
-      .then(()=>{
-        setClothingItems((prevItems)=>{
-          prevItems.filter((item)=>item._id !== selectedCard._id)
-        })
-        setDeleteConfirmation(false);
-        closeActiveModal();
-      })
-      .catch((err)=>console.log(err));
-    })
+export const defaultWeatherOpions = {
+  day:{
+    day: true,
+    condition: "clear",
+    url: new URL("../assets/day/default.svg", import.meta.url).href,
+  },
+  night:{
+    day: false,
+    condition: "clear",
+    url: new URL("../assets/night/night_default.svg", import.meta.url).href,
   }
 }
 
+export const baseUrl = process.env.NODE_ENV === "production" 
+  ? "https://api.Fit24.jumpingcrab.com"
+  : "http://localhost:3001";
 
-useEffect(()=>{
-  getWeather(coordinates, APIkey)
-  .then((data)=>{
-    console.log(data);
-    const filterData = filterWeatherData(data,urrentTemperatureUnit);
-    setWeatherdata(filterData);
-  })
-  .catch(console.error);
-
-},[])
+  
