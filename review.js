632ef4052eec266dc03ec431b0c2d3616959a2e1
sun4205 <hanna4205@gmail.com>
2025-03-04 22:28:39 -0500

    practice project

--- review.js
+++ review.js
@@ -1,32 +1,10 @@
-export const getWeather = ({latitude,longitude},APIkey) => {
-  return fetch(``).then(checkresponse)
+const TOKEN_KEY = "JWT";
+
+export const setToken = (token) => localStorage.setItem(TOKEN_KEY,token);
+export const getToken = () => {
+  return localStorage.getItem(TOKEN_KEY);
 }
 
-export const getWeatherType = (temperature, currentTemperatureUnit) => {
-  const temp = currentTemperatureUnit === "F" ? temperature.F: temperature.C;
-  if(temp>86){
-    return "hot"
-  }else if(temp >=66 && temp<=86){
-    return "warm";
-  }else {
-    return "cold";
-  }
-}
-
-export const isDay = ({sunrise, sunset}) => {
-  const now = Date.now();
-  return sunrise * 1000 < now && now < sunset * 1000;
-}
-
-export const filterWeatherData = (data, currentTemperatureUnit) => {
-  const result = {};
-  result.city = data.name;
-  result.temp = {
-    F:data.main.temp,
-    C:Math.round((data.main.temp-32)*5/9),
-  }
-  result.type = getWeatherType(result.temp, currentTemperatureUnit);
-  result.condition = data.weather[0].main.toLowercase();
-  result.isDay = isDay(data.sys);
-  return result;
+export const removeToken = () => {
+  return localStorage.removeItem(TOKEN_KEY);
 }
