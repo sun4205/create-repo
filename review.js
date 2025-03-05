94db02a517de4088064825afdb97892be624a12b
sun4205 <hanna4205@gmail.com>
2025-03-05 11:15:39 -0500

    practice project

--- review.js
+++ review.js
@@ -1,10 +1,51 @@
-const TOKEN_KEY = "JWT";
+const {values, handleChange} = useForm({
+  name:"",
+  imageUrl:"",
+  weather:"",
+})
 
-export const setToken = (token) => localStorage.setItem(TOKEN_KEY,token);
-export const getToken = () => {
-  return localStorage.getItem(TOKEN_KEY);
+const handleSubmit = (e) =>{
+  e.preventDefault();
+  handleAddItemSubmit(values);
 }
 
-export const removeToken = () => {
-  return localStorage.removeItem(TOKEN_KEY);
+function asyncSubmit(request){
+  setIsLoading(true);
+  request()
+  .then(closeActiveModal)
+  .catch(console.error)
+  .fianlly(()=>setIsLoading(false));
+  
+}
+
+const handleAddItemSubmit =(item)=>{
+  asyncSubmit(()=>{
+    addItem(item).then((newItem)=>{ 
+      setClothingItem([newItem, ...clothingItme]);
+    })
+  })
+}
+
+function checkResponse(res) {
+  return res.ok?res.json() : Promise.reject(`Error:${res.status}`);
+}
+
+function request(url, options){
+  return fetch(url,options).then(checkResponse);
+}
+
+const addItem = ({name, weather, imageUrl}) => {
+  const token = localStorage.getItem('jwt');
+  return request(`${baseUrl}/items`,{
+    method:"POST",
+    headers:{
+      "Content-Type":"application/json",
+      authorization:`Bearer ${token}`,
+    },
+    body:JSON.stringify({
+      name,
+      weather,
+      imageUrl,
+    })
+  })
 }
