7246598d742e767557f02e78361ad292d7a85d9a
sun4205 <hanna4205@gmail.com>
2025-03-04 21:24:43 -0500

    practice project

--- review.js
+++ review.js
@@ -1,102 +1,32 @@
-export const weatherOptions = [
-  {
-    day: true,
-    condition: "sunny",
-    url: new URL("../assets/day/sunny.svg", import.meta.url).href,
-  },
-  {
-    day: true,
-    condition: "clouds",
-    url: new URL("../assets/day/cloudy.svg", import.meta.url).href,
-  },
-]
+export const getWeather = ({latitude,longitude},APIkey) => {
+  return fetch(``).then(checkresponse)
+}
 
-export const defaultWeatherOpions = {
-  day:{
-    day: true,
-    condition: "clear",
-    url: new URL("../assets/day/default.svg", import.meta.url).href,
-  },
-  night:{
-    day: false,
-    condition: "clear",
-    url: new URL("../assets/night/night_default.svg", import.meta.url).href,
+export const getWeatherType = (temperature, currentTemperatureUnit) => {
+  const temp = currentTemperatureUnit === "F" ? temperature.F: temperature.C;
+  if(temp>86){
+    return "hot"
+  }else if(temp >=66 && temp<=86){
+    return "warm";
+  }else {
+    return "cold";
   }
 }
 
-export const baseUrl = process.env.NODE_ENV === "production" 
-  ? "https://api.Fit24.jumpingcrab.com"
-  : "http://localhost:3001";
-
-function checkResponse(res){
-  return res.ok? res.json() :Promise.reject(`Error:${res.status}`);
+export const isDay = ({sunrise, sunset}) => {
+  const now = Date.now();
+  return sunrise * 1000 < now && now < sunset * 1000;
 }
 
-function request(url,options){
-  return fetch(url,options).then(checkResponse);
-}
-
-function getItem(){
-  return request(`${baseUrl}/item`);
-}
-
-const addItem = ({name,weather,imageUrl})=>{
-  return request(`${baseUrl}/item`,{
-    method:"POST",
-    headers:{
-      "Content-type":"application/json",
-      authorization:`Bearer ${token}`,
-    },
-    body:JSON.stringify({
-      name,
-      weather,
-      imageUrl,
-    }),
-  });
-};
-
-const updatedUserData = (username,avatar)=>{
-  const token = localStorage.getItem("jwt");
-  if(!token) return;
-  if(!username || !avatar) return;
-  return fetch(`${baseUrl}/users/me`,{
-    method:"PATCH",
-    headers:{
-      "Content-type":"application/json",
-      authorization:`Bearer${token}`,
-    },
-    body:JSON.stringify({name:username, avatar:avatar||""}),
-  })
-  .then((res)=>{
-    if(!res.ok){
-      throw new Error(`failed to updated user data`)
-    }
-    return res.json();
+export const filterWeatherData = (data, currentTemperatureUnit) => {
+  const result = {};
+  result.city = data.name;
+  result.temp = {
+    F:data.main.temp,
+    C:Math.round((data.main.temp-32)*5/9),
   }
-  )
-  .catch((error)=>console.error(error.messge))
+  result.type = getWeatherType(result.temp, currentTemperatureUnit);
+  result.condition = data.weather[0].main.toLowercase();
+  result.isDay = isDay(data.sys);
+  return result;
 }
-
-const addCardLike = (id,token)=>{
-  return fetch(`${baseUrl}/items/${id}/likes`,{
-    method:"PUT",
-    headers:{
-      "Content-Type":"application/json",
-      authorization:`Bearer ${token}`,
-    }    
-  })
-  .then((res)=>res.json())
-    .catch((err)=>console.log(err))
-};
-
-const removeCardLike = (id, token) => {
-  return fetch(`${baseUrl}/items/${id}/likes`,{
-    method:"DELETE",
-    haeders:{
-      "Content-Type":"application/json",
-      authorization:`Bearer ${token}`,
-    }
-  })
-  .then((res)=>res.json())
-  .catch((err)=>console.log(err))
-};
