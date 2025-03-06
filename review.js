4078ad2c757997e782608ff211e622e8597f1de4
sun4205 <hanna4205@gmail.com>
2025-03-06 11:38:21 -0500

    practice project

--- review.js
+++ review.js
@@ -1,15 +1,37 @@
-return (
-  <header className = "header">
-   
-    <Link to = "/">
-    <img src={logo} className="header__logo" alt={logo}/>
-    </Link>
-    <p className="header__date-and-location">{currentDate},{weatherData.city}</p>
-    <div className="header_controls">
-      <toggleSwitch />
-      <button className="header__add-btn" onClick={handleChange}>add button</button>
-      <button className="header__user-name" onClick={}></button>
-    </div>
-   
-  </header>
-)
+import { useContext } from "react";
+
+const ToggleSwitch = () => {
+  const { currentTemperatureUnit, handleToggleSwitchChange } = useContext(
+    currentTemperatureUnitContext
+  );
+  return (
+    <label className="switch">
+      <input
+        className="switch__box"
+        type="checkbox"
+        onChange={handleToggleSwitchChange}
+      />
+      <span
+        className={
+          currentTemperatureUnit === "F"
+            ? "switch__slider switch__slider-F"
+            : "switch__slider switch slider-C"
+        }
+      ></span>
+      <p
+        className={`switch__temp-C ${
+          currentTemperatureUnit === "C" && "switch__active"
+        }`}
+      >
+        C
+      </p>
+      <p
+        className={`switch__temp-F ${
+          currentTemperatureUnit === "F" && "switch__active"
+        }`}
+      >
+        F
+      </p>
+    </label>
+  );
+};
