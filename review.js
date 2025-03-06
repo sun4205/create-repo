8dcc58481b322c1f61badef8cfe74c365f843455
sun4205 <hanna4205@gmail.com>
2025-03-06 11:44:42 -0500

    practice project

--- review.js
+++ review.js
@@ -1,37 +1,24 @@
-import { useContext } from "react";
+const useEscapeKey = (isActive, closeActiveModal, modalRef) => {
+  useEffect (()=>{
+    if(!isActive) return;
+    const handleEscClose = (e) => {
+      if(e.key === "Escape"){
+        closeActiveModal();
+      }
+    };
 
-const ToggleSwitch = () => {
-  const { currentTemperatureUnit, handleToggleSwitchChange } = useContext(
-    currentTemperatureUnitContext
-  );
-  return (
-    <label className="switch">
-      <input
-        className="switch__box"
-        type="checkbox"
-        onChange={handleToggleSwitchChange}
-      />
-      <span
-        className={
-          currentTemperatureUnit === "F"
-            ? "switch__slider switch__slider-F"
-            : "switch__slider switch slider-C"
-        }
-      ></span>
-      <p
-        className={`switch__temp-C ${
-          currentTemperatureUnit === "C" && "switch__active"
-        }`}
-      >
-        C
-      </p>
-      <p
-        className={`switch__temp-F ${
-          currentTemperatureUnit === "F" && "switch__active"
-        }`}
-      >
-        F
-      </p>
-    </label>
-  );
-};
+    const handleOverlayClick = (e) => {
+      if(modalRef?.current && e.target === modalRef.current){
+        closeActiveModal();
+      }else{}
+      
+    };
+
+    return () => {
+      document.removeEventListener("keydown",handleEscClose);
+      document.removeEventListener("click",handleOverlayClick);
+    }
+    
+
+  },[isActive,closeActiveModal,modalRef]);
+}
