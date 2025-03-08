ea86436d0e5f1c23ecaa9794f238fb202c68de05
sun4205 <hanna4205@gmail.com>
2025-03-07 20:17:23 -0500

    practice review

--- review.js
+++ review.js
@@ -1,24 +1,21 @@
-const useEscapeKey = (isActive, closeActiveModal, modalRef) => {
-  useEffect (()=>{
-    if(!isActive) return;
-    const handleEscClose = (e) => {
-      if(e.key === "Escape"){
-        closeActiveModal();
-      }
+
+export const getWeather = ({ latitude, longitude }, APIkey) => {
+    return fetch(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=imperial&appid=${APIkey}`
+    ).then(checkResponse);
+  };
+  
+  export const filterWeatherData = (data, currentTemperatureUnit) => {
+    const result = {};
+    result.city = data.name;
+    result.temp = {
+      F: data.main.temp,
+      C: Math.round(((data.main.temp - 32) * 5) / 9),
     };
-
-    const handleOverlayClick = (e) => {
-      if(modalRef?.current && e.target === modalRef.current){
-        closeActiveModal();
-      }else{}
-      
-    };
-
-    return () => {
-      document.removeEventListener("keydown",handleEscClose);
-      document.removeEventListener("click",handleOverlayClick);
-    }
-    
-
-  },[isActive,closeActiveModal,modalRef]);
-}
+    console.log("Temperature object:", result.temp);
+    result.type = getWeatherType(result.temp, currentTemperatureUnit);
+    result.condition = data.weather[0].main.toLowerCase();
+    result.isDay = isDay(data.sys);
+    return result;
+  };
+  
