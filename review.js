e93e696b6a5fa67aa8041116bb7d2dc06c078026
sun4205 <hanna4205@gmail.com>
2025-03-12 21:06:54 -0400

    practice project

--- review.js
+++ review.js
@@ -1,15 +1,68 @@
-import { useState } from "react";
+import React, { useRef } from "react";
+import "./LoginModal.css";
+import ModalWithForm from "../ModalWithForm/ModalWithForm";
+import { useForm } from "../../hooks/useForm";
 
-export function useForm(initialValues) {
-  const [values, setValues] = useState(initialValues);
+function LoginModal(
+  activeModal,
+  closeActiveModal,
+  buttonText = "Login",
+  handleLogin,
+  setActiveModal,
+  modalRef
+) {
+  const { values, handleChange } = useForm({
+    email: "",
+    password: "",
+  });
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setValues((preValues)=>({
-      ...preValues,
-      [name]:value,
-    }));
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Form submitted with values:", values);
+    handleLogin({
+      email: values.email,
+      password: values.password,
+    });
   };
+  return (
+    <ModalWithForm
+      isOpen={activeModal === "login"}
+      title="Login"
+      buttonText={buttonText}
+      secondaryButtonText="or Sign up"
+      onSecondaryClick={() => setActiveModal("register")}
+      activeModal={activeModal}
+      onSubmit={handleSubmit}
+      modalRef={modalRef}
+      closeActiveModal={closeActiveModal}
+    >
+      <label htmlFor="email" className="modal__label">
+        Email
+        <input
+          type="email"
+          className="modal__input"
+          id="email"
+          name="email"
+          placeholder="Email"
+          value={values.email}
+          onChange={handleChange}
+        />
+      </label>
 
-  return { values, handleChange, setValues };
+      <label htmlFor="password" className="modal__label">
+        Password
+        <input
+          type="password"
+          className="modal__input"
+          id="password"
+          name="password"
+          placeholder="Password"
+          vlaue={values.password}
+          onChange={handleChange}
+        />
+      </label>
+    </ModalWithForm>
+  );
 }
+
+export default LoginModal;
