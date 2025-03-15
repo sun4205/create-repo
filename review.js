4a4b76c539d02f782918844d2be7b93d01229164
sun4205 <hanna4205@gmail.com>
2025-03-15 19:47:43 -0400

    practice project

--- review.js
+++ review.js
@@ -1,13 +1,12 @@
-import './ShowMore.css';
-import showmorebtn from '../../images/Rectangle.svg';
+import './savedArticles.css';
 
-function ShowMore(){
+function SavedArticles() {
     return(
-        <div className='showmore__container'>
-            <img src={showmorebtn} className='showmore__btn' alt="showmore"></img>
-            <span className='showmore__text'>Show More</span>
+        <div className='savedArticles__container'>
+            <p className='savedArticles__title'>Saved articles</p>
+            <p className='savedArticles__numberSaved'>you have 5 saved articles</p>
+            <p className='savedArticles__by'>By keywords: <span className='savedArticles__kewords'>Nature, Yellowstone, and 2 other</span></p>
         </div>
     )
 }
-
-export default ShowMore;
+export default SavedArticles;
