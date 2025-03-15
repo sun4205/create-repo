1b1d5ab90e735d01621924640508cec4d235a957
sun4205 <hanna4205@gmail.com>
2025-03-14 21:09:30 -0400

    practice project

--- review.js
+++ review.js
@@ -1,8 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Navigation.css";
+import Union from "../../images/Union.svg";
+import CurrentUserContext from "../../contexts/CurrentUserContext";
+import { useContext } from "react";
 
-const CurrentUserContext = React.createContext({
-  currentUser: null,
-  setCurrentUser: () => {},
-});
 
-export default CurrentUserContext;
+function Navigation({ openLoginModal }) {
+  const { currentUser, setCurrentUser } = useContext(CurrentUserContext);
+
+  return (
+    <div className="navigation__nav">
+      <button type="button" className="navigation__home-btn">
+        Home
+      </button>
+      {!currentUser ? (
+        <button
+          type="button"
+          onClick={openLoginModal}
+          className="navigation__signIn-btn"
+        >
+          Sign In
+        </button>
+      ) : (
+        <div className="navigation__loggedIn-control">
+          <a href="/savedArticle">
+            <button type="button" className="navigation__savedArticle-nav">
+              Saved Articles
+            </button>
+          </a>
+
+          <div className="navigation__username">
+            {currentUser.username}
+            <button type="button">
+              <img src={Union} className="navigation__logout" />
+            </button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Navigation;
