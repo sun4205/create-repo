b29a0616622eddd7877cf3956190d1775a5f9613
sun4205 <hanna4205@gmail.com>
2025-03-15 19:07:16 -0400

    practice project

--- review.js
+++ review.js
@@ -1,25 +1,13 @@
-import "./NewsCard.css";
-import savebtn from "../../images/savebtn.svg";
-import nature from"../../images/nature.svg";
+import './ShowMore.css';
+import showmorebtn from '../../images/Rectangle.svg';
 
-
-function NewsCard(data) {
-  
-  return (
-   
-    <li className="card">
-      <div className="card__image-control">
-      <img className="card__image" src={nature} alt="nature" />
-      <button className="card__save-btn"><img src={savebtn}className="card__savebtn-img"></img></button>
-      </div>
-      <div className="card__info">
-      <p className="card__date">November 4, 2020</p>
-      <p className="card__title">Everyone Needs a Special 'Sit Spot' in Nature</p>
-      <p className="card__description">Ever since I read Richard Louv's influential book, "Last Child in the Woods," the idea of having a special "sit spot" has stuck with me. This advice, which Louv attributes to nature educator Jon Young, is for both adults and children to find...</p>
-      <p className="card__source">treehugger</p>
-      </div>
-    </li>
-  );
+function ShowMore(){
+    return(
+        <div className='showmore__container'>
+            <img src={showmorebtn} className='showmore__btn' alt="showmore"></img>
+            <span className='showmore__text'>Show More</span>
+        </div>
+    )
 }
 
-export default NewsCard;
+export default ShowMore;
