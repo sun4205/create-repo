2f2067cfcb58a02dbc8ad8df39800d04b762a6ed
sun4205 <hanna4205@gmail.com>
2025-03-18 23:21:44 -0400

    practice project

--- review.js
+++ review.js
@@ -1,12 +1,56 @@
-import './savedArticles.css';
+import React, { useState } from "react";
+import "./Navigation.css";
+import { useNavigate } from "react-router-dom";
+import Union from "../../images/Union.svg";
+import CurrentUserContext from "../../contexts/CurrentUserContext";
+import { useContext } from "react";
 
-function SavedArticles() {
-    return(
-        <div className='savedArticles__container'>
-            <p className='savedArticles__title'>Saved articles</p>
-            <p className='savedArticles__numberSaved'>you have 5 saved articles</p>
-            <p className='savedArticles__by'>By keywords: <span className='savedArticles__kewords'>Nature, Yellowstone, and 2 other</span></p>
+function Navigation({ openLoginModal, handleLogOut }) {
+  const { currentUser, setCurrentUser } = useContext(CurrentUserContext);
+  const navigate = useNavigate();
+
+  const handleHomeClick = () => {
+    navigate("/");
+  };
+
+
+  const handleSavedNews = () => {
+    navigate("/savedNews");
+  };
+
+  return (
+    <div className="navigation__nav">
+      <button onClick ={handleHomeClick} type="button" className="navigation__home-btn">
+        Home
+      </button>
+      {!currentUser ? (
+        <button
+          type="button"
+          onClick={openLoginModal}
+          className="navigation__signIn-btn"
+        >
+          Sign In
+        </button>
+      ) : (
+        <div className="navigation__loggedIn-control">
+          <button
+            onclick={handleSavedNews}
+            type="button"
+            className="navigation__savedArticle-nav"
+          >
+            Saved Articles
+          </button>
+
+          <div className="navigation__username">
+            {currentUser.username}
+            <button onClick={handleLogOut} type="button">
+              <img src={Union} className="navigation__logout" />
+            </button>
+          </div>
         </div>
-    )
+      )}
+    </div>
+  );
 }
-export default SavedArticles;
+
+export default Navigation;
