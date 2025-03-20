382931667594f70e8826c78309dedcab049f507a
sun4205 <hanna4205@gmail.com>
2025-03-19 21:35:22 -0400

    practice project

--- review.js
+++ review.js
@@ -1,13 +1,17 @@
-import './ShowMore.css';
-import showmorebtn from '../../images/Rectangle.svg';
+import { useState } from 'react';
+import { useDebounce } from '../../utils/useDebounce';
+import { fetchData } from '../../utils/apiService';
 
-function ShowMore(){
-    return(
-        <div className='showmore__container'>
-            <img src={showmorebtn} className='showmore__btn' alt="showmore"></img>
-            <span className='showmore__text'>Show More</span>
-        </div>
-    )
-}
+const SearchComponent = () => {
+  const [query, setQuery] = useState('');
+  const debouncedFetchData = useDebounce((q) => fetchData(q), 500);
 
-export default ShowMore;
+  const handleInputChange = (event) => {
+    setQuery(event.target.value);
+    debouncedFetchData(event.target.value);
+  };
+
+  return <input type="text" value={query} onChange={handleInputChange} />;
+};
+
+export default SearchComponent;
