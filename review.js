85953d20719674975e36dd5ee61ee314fa7cd1bb
sun4205 <hanna4205@gmail.com>
2025-03-19 21:34:23 -0400

    practice project

--- review.js
+++ review.js
@@ -1,56 +1,13 @@
-import React, { useState } from "react";
-import "./Navigation.css";
-import { useNavigate } from "react-router-dom";
-import Union from "../../images/Union.svg";
-import CurrentUserContext from "../../contexts/CurrentUserContext";
-import { useContext } from "react";
+import './ShowMore.css';
+import showmorebtn from '../../images/Rectangle.svg';
 
-function Navigation({ openLoginModal, handleLogOut }) {
-  const { currentUser, setCurrentUser } = useContext(CurrentUserContext);
-  const navigate = useNavigate();
-
-  const handleHomeClick = () => {
-    navigate("/");
-  };
-
-
-  const handleSavedNews = () => {
-    navigate("/savedNews");
-  };
-
-  return (
-    <div className="navigation__nav">
-      <button onClick ={handleHomeClick} type="button" className="navigation__home-btn">
-        Home
-      </button>
-      {!currentUser ? (
-        <button
-          type="button"
-          onClick={openLoginModal}
-          className="navigation__signIn-btn"
-        >
-          Sign In
-        </button>
-      ) : (
-        <div className="navigation__loggedIn-control">
-          <button
-            onclick={handleSavedNews}
-            type="button"
-            className="navigation__savedArticle-nav"
-          >
-            Saved Articles
-          </button>
-
-          <div className="navigation__username">
-            {currentUser.username}
-            <button onClick={handleLogOut} type="button">
-              <img src={Union} className="navigation__logout" />
-            </button>
-          </div>
+function ShowMore(){
+    return(
+        <div className='showmore__container'>
+            <img src={showmorebtn} className='showmore__btn' alt="showmore"></img>
+            <span className='showmore__text'>Show More</span>
         </div>
-      )}
-    </div>
-  );
+    )
 }
 
-export default Navigation;
+export default ShowMore;
