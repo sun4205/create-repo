22a92e3b324f6311a708a264de215c6fc1601c30
sun4205 <hanna4205@gmail.com>
2025-03-21 21:56:47 -0400

    practice project

--- review.js
+++ review.js
@@ -1,51 +1,31 @@
-import { useEffect } from "react";
-import "./SearchForm.css";
-import { useState } from "react";
+import "./Main.css";
+import NewsCard from "../NewsCard/NewsCard";
+import About from "../About/About";
+import ShowMore from "../ShowMore/ShowMore";
 
-function SearchForm({ handleSearchSubmit,query,setQuery }) {
-  const [debouncedQuery, setDebouncedQuery] = useState(""); 
 
-  useEffect(()=>{
-    const timer =setTimeout(()=>{
-      setDebouncedQuery(query);
-    },500);
-    return () => clearTimeout(timer);
-  },[query]);
-
-  useEffect(() => {
-    if (debouncedQuery.trim() !== "") {
-      handleSearchSubmit(debouncedQuery);
-    }
-  }, [debouncedQuery]);
- 
-    const handleInputChange = (e) => {
-        setQuery(e.target.value);
-        console.log("setquery:", e.target.value); 
-    }
-
+function Main({newsData,newsItems}) {
+  console.log("newsItems in Main:", newsItems);
   return (
-    <div className="searchForm__container">
-      <h1 className="searchForm__title">What's going on in the world?</h1>
-      <p className="searchForm__description">
-        Find the latest news on any topic and save them in your personal
-        account.
-      </p>
-      <div className="searchForm__controls">
-        <label className="searchForm__label">
-          <input
-            className="searchForm__input"
-            type="text"
-            id="search__input"
-            name="search__input"
-            placeholder="Enter Topic"
-            value={query}
-            onChange = {handleInputChange}
-          ></input>
-        </label>
-        <button type="button" onClick={handleSubmit} className="searchForm__btn"></button>
-      </div>
-    </div>
+    <main className="main">
+      <h2 className="main__title">Search results</h2>
+      <section className="main__cards">
+        <ul className="main__cards-list">
+        {Array.isArray(newsItems.articles) && newsItems.articles.length > 0 ? (
+            newsItems.articles.slice(0, 9).map((item, index) => (
+              <NewsCard key={index} data={item} />
+            ))
+          ) : (
+            <p>No news items available</p>
+          )}
+                 
+        </ul>
+        <ShowMore />
+        <About />
+       
+      </section>
+    </main>
   );
 }
 
-export default SearchForm;
+export default Main;
