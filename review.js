962bfc859c401d049da2440fccd0d9975f8a21f2
sun4205 <hanna4205@gmail.com>
2025-03-23 22:45:19 -0400

    practice project

--- review.js
+++ review.js
@@ -1,31 +1,39 @@
-import "./savedArticles.css";
-import NewsCard from "../NewsCard/NewsCard";
-import CurrentUserContext from "../../contexts/CurrentUserContext";
+import React from "react";
+import close from "../../images/close.svg";
+import "./RegisterMessage.css";
 
-function SavedArticles({ savedArticles }) {
-  console.log("savedArticles:", savedArticles);
-  const { currentUser, setCurrentUser } = useContext(CurrentUserContext);
-    return(
-         <div className='savedArticles__container'>
-             <p className='savedArticles__title'>Saved articles</p>
-             <p className='savedArticles__numberSaved'>{currentUser.username}, you have {savedArticles.length} saved articles</p>
-            <p className='savedArticles__by'>By keywords: <span className='savedArticles__keywords'> {savedArticles.map(article => article.keywords).join(", ")}</span></p>
+function RegisterMessage({
+  activeModal,
+  closeActiveModal,
+  setActiveModal,
+  modalRef,
+}) {
+  return (
+    <div className="modal modal_opened">
+      <div className="modal__content modal__register_msg">
+        <h2 className="modal__title modal__register_title">Registration Successfully Completed!</h2>
+        <button
+          onClick={() => {
+            console.log("closeActiveModal called");
+            closeActiveModal();
+          }}
+          type="button"
+          className="modal__close"
+        >
+          <img src={close} className="modal__close-btn" alt="close_button" />
+        </button>
+        <button
+          onClick={() => {
+            setActiveModal("login"); 
+          }}
+          type="button"
+          className="signup-text modal__register_btn"
+        >
+          Sign in
+        </button>
+      </div>
+    </div>
+  );
+}
 
-             <ul className='savedArticles__lists'>
-            {savedArticles.map((article, index) => (
-           <NewsCard
-             key={index}
-            image={article.urlToImage}
-             date={article.date}
-             title={article.title}
-             description={article.description}
-             source={article.source.name}
-           saveBtnImage={article.saveBtnImage}
-          />
-         ))}
-             </ul>
-         </div>
-    )
-  }
-
-export default SavedArticles;
+export default RegisterMessage;
