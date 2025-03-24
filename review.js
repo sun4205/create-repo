84bc2369f178ce4d1b885ad15d0ef2b9f1bc089a
sun4205 <hanna4205@gmail.com>
2025-03-23 22:45:59 -0400

    project practice

--- review.js
+++ review.js
@@ -1,39 +1,101 @@
-import React from "react";
-import close from "../../images/close.svg";
-import "./RegisterMessage.css";
+import "./RegisterModal.css";
+import React, { useRef, useState} from "react";
+import ModalWithForm from "../ModalWithForm/ModalWithForm";
+import RegisterMessage from "../RegisterMessage/RegisterMessage";
+import { useForm } from "../../hooks/useForm";
 
-function RegisterMessage({
+function RegisterModal({
   activeModal,
   closeActiveModal,
+  handleRegisterSubmit,
   setActiveModal,
   modalRef,
+  buttonText,
 }) {
+  const { values, handleChange } = useForm({
+    email: "",
+    password: "",
+    username: "",
+  });
+
+  const [isSuccess, setIsSuccess] = useState(false);
+
+  const isFilled = values.email.trim() !== "" || values.password.trim() !== "";
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Form submitted with values:", values);
+    handleRegisterSubmit({
+      email: values.email,
+      password: values.password,
+      username: values.username,
+    });
+    setIsSuccess(true);
+  };
   return (
-    <div className="modal modal_opened">
-      <div className="modal__content modal__register_msg">
-        <h2 className="modal__title modal__register_title">Registration Successfully Completed!</h2>
-        <button
-          onClick={() => {
-            console.log("closeActiveModal called");
-            closeActiveModal();
-          }}
-          type="button"
-          className="modal__close"
-        >
-          <img src={close} className="modal__close-btn" alt="close_button" />
-        </button>
-        <button
-          onClick={() => {
-            setActiveModal("login"); 
-          }}
-          type="button"
-          className="signup-text modal__register_btn"
-        >
-          Sign in
-        </button>
-      </div>
-    </div>
+    <> {isSuccess ?(
+      <RegisterMessage closeActiveModal={closeActiveModal} setActiveModal={setActiveModal}/>
+    ) : (
+    <ModalWithForm
+      isOpen={activeModal === "register"}
+      title={isSuccess ? "Registration Successfully Completed" : "Sign Up"}
+      secondaryButtonText={
+        <>
+          <span className="or-text">or</span>{" "}
+          <span className="signup-text">Sign in</span>
+        </>
+      }
+      onSecondaryClick={() => setActiveModal("login")}
+      activeModal={activeModal}
+      onSubmit={handleSubmit}
+      modalRef={modalRef}
+      closeActiveModal={closeActiveModal}
+      customClass="modal__signIn"
+      isFilled={isFilled}
+    >
+      
+         <label htmlFor="email" className="modal__label">
+        Email
+        <input
+          type="email"
+          className="modal__input"
+          id="email"
+          name="email"
+          placeholder="Enter Email"
+          value={values.email}
+          onChange={handleChange}
+        />
+      </label>
+
+      <label htmlFor="password" className="modal__label">
+        Password
+        <input
+          type="password"
+          className="modal__input"
+          id="password"
+          name="password"
+          placeholder="Enter Password"
+          value={values.password}
+          onChange={handleChange}
+        />
+      </label>
+
+      <label htmlFor="username" className="modal__label">
+        Username
+        <input
+          type="username"
+          className="modal__input"
+          id="username"
+          name="username"
+          placeholder="Enter your username"
+          value={values.username}
+          onChange={handleChange}
+        />
+      </label>
+    </ModalWithForm>
+    )}
+    </>
   );
 }
 
-export default RegisterMessage;
+export default RegisterModal;
