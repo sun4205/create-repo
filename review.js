3f96014d88a56d4ab38979b3d1d0cb697bb562b5
sun4205 <hanna4205@gmail.com>
2025-03-24 20:34:13 -0400

    practice project

--- review.js
+++ review.js
@@ -1,30 +1,12 @@
-import { useEffect } from "react";
+import './Preloader.css';
 
-const useEscapeKey = (isActive, closeActiveModal, modalRef) => {
-  useEffect(() => {
-    if (!isActive) return;
+function Preloader() {
+    return(
+        <div className='preloader__container'>
+        <div className='circle-preloader'></div>
+        <p className='preloader__paragraph'>Searching For News...</p>
+        </div>
+    )
+}
 
-    const handleEscClose = (e) => {
-      if (e.key === "Escape") {
-        closeActiveModal();
-      }
-    };
-
-    const handleOverlayClick = (e) => {
-      if (modalRef?.current && e.target === modalRef.current) {
-        closeActiveModal();
-      } else {
-      }
-    };
-
-    document.addEventListener("keydown", handleEscClose);
-    document.addEventListener("click", handleOverlayClick);
-
-    return () => {
-      document.removeEventListener("keydown", handleEscClose);
-      document.removeEventListener("click", handleOverlayClick);
-    };
-  }, [isActive, closeActiveModal, modalRef]);
-};
-
-export default useEscapeKey;
+export default Preloader;
