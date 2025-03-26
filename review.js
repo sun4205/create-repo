cd9b166385bc23cf385597728d9cfbc6f2ab7f25
sun4205 <hanna4205@gmail.com>
2025-03-25 22:27:36 -0400

    practice project

--- review.js
+++ review.js
@@ -1,12 +1,23 @@
-import './Preloader.css';
+const express = require("express");
+const router = express.Router();
+const auth = require("../middlewares/auth");
+const {
+  savedArticle,
+  getSavedArticles,
+  deleteArticle,
+} = require("../controller/savedNews");
 
-function Preloader() {
-    return(
-        <div className='preloader__container'>
-        <div className='circle-preloader'></div>
-        <p className='preloader__paragraph'>Searching For News...</p>
-        </div>
-    )
-}
+router.get("/saveNews", auth, getSavedArticles);
+router.post("/saveNews", auth, savedArticle);
+router.put("/saveNews/:id", auth, (req, res, next) => {
+  const articleId = decodeURIComponent(req.params.id);
+  req.params.id = articleId;
+  savedArticle(req, res, next);
+});
+router.delete("/saveNews/:id", auth, (req, res, next) => {
+  const decodedId = decodeURIComponent(req.params.id);
+  req.params.id = decodedId;
+  deleteArticle(req, res, next);
+});
 
-export default Preloader;
+module.exports = router;
