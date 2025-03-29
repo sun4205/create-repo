fda49adaeb5f29a9f8ce01d4f2403263d32920c6
sun4205 <hanna4205@gmail.com>
2025-03-29 16:10:58 -0400

    review

--- review.js
+++ review.js
@@ -1,71 +1,269 @@
-const handleNewsSaved = ({ data }) => {
-   
-  const token = localStorage.getItem("jwt");
-  const articleId = crypto.randomUUID();
-  const saved = data.saved;
-  const { source, title, date, description, image } = data;
+import React, { useEffect, useState } from "react";
+import { Routes, Route, useNavigate, useLocation } from "react-router-dom";
+import "./App.css";
+import Header from "../Header/Header";
+import Main from "../Main/Main";
+import * as auth from "../../utils/auth";
+import * as api from "../../utils/api";
+import * as newsapi from "../../utils/NewsApi";
+import * as token from "../../utils/token";
+import LoginModal from "../LoginModal/LoginModal";
+import RegisterModal from "../RegisterModal/RegisterModal";
+import Footer from "../Footer/Footer";
+import CurrentUserContext from "../../contexts/CurrentUserContext";
+import SavedArticlesContext from "../../contexts/SavedArticlesContext";
+import SavedArticles from "../SavedArticles/SavedArticles";
+import ProtectedRoute from "../ProtectedRoute/ProtectedRoute";
+import About from "../About/About";
+import RegisterMessage from "../RegisterMessage/RegisterMessage";
 
-  const currentArticle = Array.isArray(newsItems.articles)
-    ? newsItems.articles
-    : [];
-    console.log("currentarticle", currentArticle);
- 
-  const currentSavedArticles =
-    JSON.parse(localStorage.getItem("savedArticles")) || [];
-   
-    const isArticleAlreadySaved = currentSavedArticles.some((item) => item.id === articleId);
+function App() {
+  const [query, setQuery] = useState("");
+  const [newsData, setNewsData] = useState({
+    source: "",
+    title: "",
+    date: "",
+    description: "",
+    image: "",
+  });
+  const [activeModal, setActiveModal] = useState("");
+  const [newsItems, setNewsItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [currentUser, setCurrentUser] = useState(null);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [jwt, setJwt] = useState(token.getToken());
+  const [savedArticles, setSavedArticles] = useState([]);
 
+  const navigate = useNavigate();
+  const location = useLocation();
 
-  if (!Array.isArray(currentArticle)) {
-    console.error(" newsItems is not an array!", newsItems);
-    return;
+  const openLoginModal = () => {
+    console.log("opening login modal ");
+    setActiveModal("login");
+  };
+
+  const closeActiveModal = () => {
+    setActiveModal("");
+  };
+
+  function asyncSubmit(request) {
+    setIsLoading(true);
+    request()
+      .then(closeActiveModal)
+
+      .catch(console.error)
+
+      .finally(() => setIsLoading(false));
   }
 
-  if (isArticleAlreadySaved) {
-    console.log("This article is already saved.");
-    return; 
+  function getUserInformation(token) {
+    return auth
+      .getUserInfo(token)
+      .then((user) => {
+        setIsLoggedIn(true);
+        setCurrentUser(user);
+      })
+      .catch(console.error);
   }
 
-  if (!saved) {
+  const handleLogin = ({ email, password }) => {
+    if (!email || !password) {
+      return;
+    }
 
+    auth
+      .authorize(email, password)
+      .then((data) => {
+        if (data.token) {
+          token.setToken(data.token);
+          setJwt(data.token);
+          setIsLoggedIn(true);
+          console.log("Login successful!.");
+          getUserInformation(data.token).then((userInfo) => {
+            setCurrentUser(userInfo);
+            console.log("User info updated:", userInfo);
+            const redirectPath = location.state?.from?.pathname || "/";
+            console.log("redirectPath", redirectPath);
+            navigate(redirectPath);
+            closeActiveModal();
+          });
+        }
+      })
+      .catch(console.error);
+  };
 
-     api
+  useEffect(() => {
+    const jwtFromStorage = token.getToken();
+    console.log("JWT from storage:", jwtFromStorage);
 
-        .savedNews({ source, title, date, description, image })
+    if (!jwtFromStorage) {
+      console.log("No JWT found. Logging out.");
+      setCurrentUser(null);
+      setIsLoggedIn(false);
+      return;
+    }
+
+    auth.getUserInfo(jwtFromStorage).then((userInfo) => {
+      setCurrentUser(userInfo);
+      setIsLoggedIn(true);
+      console.log("userInfo", userInfo);
+    });
+  }, [jwt]);
+
+  const handleLogOut = () => {
+    token.removeToken();
+    setIsLoggedIn(false);
+    setCurrentUser(null);
+    setJwt(null);
+    navigate("/");
+    console.log("User logged out successfully.");
+  };
+
+  const handleNewsSaved = (data) => {
+    const token = localStorage.getItem("jwt");
+    console.log("data",data);   
+    console.log("dataidapp",data.data.id);
+   
+
+    const articleId = data.data.id;
+    console.log("articleId",articleId)
+    
+    if (!savedArticles.some((item) => item.id === articleId)) {
+      api.savedNews(data)
         .then((updatedData) => {
-          setNewsItems({
-            ...newsItems,
-            articles: [...currentArticle, updatedData],
-          });
-          setSavedArticles([...currentSavedArticles, updatedData]);
-          localStorage.setItem(
-            "savedArticles",
-            JSON.stringify([...currentSavedArticles, updatedData])
-          );
-          console.log("saving was successfull", updatedData);
-          console.log("currentsavedarticle",currentSavedArticles);
+          setSavedArticles((prevSavedArticles) => [...prevSavedArticles, updatedData]); 
+          console.log("updateddataa", updatedData);
+          console.log("Save successful:", updatedData);
         })
-        .catch((err) => console.log(err))
-       }else{ api
+        .catch(console.error);
+    } 
+  };
 
-        .removeNewsCardSved(articleId, token)
-        .then(() => {
-          const updatedSavedArticles = currentSavedArticles.filter(
-            (item) => item._id !== articleId
-          );
-          setNewsItems({
-            ...newsItems,
-            articles: currentArticle.filter(
-              (item) => item._id !== articleId
-            ),
-          });
-          setSavedArticles(updatedSavedArticles);
-          localStorage.setItem(
-            "savedArticles",
-            JSON.stringify(updatedSavedArticles)
-          );
-          console.log("delete complete");
-        })
-        .catch((err) => console.log(err));
-      }
-};
+  const handleRemoveArticle = (articleId) => {
+    const currentSavedArticles =
+      JSON.parse(localStorage.getItem("savedArticles")) || [];
+    const updatedSavedArticles = currentSavedArticles.filter(
+      (item) => item.id !== articleId
+    );
+    localStorage.setItem("savedArticles", JSON.stringify(updatedSavedArticles));
+    setSavedArticles(updatedSavedArticles);
+  };
+
+  const handleSearchSubmit = (values) => {
+    console.log("handleSearchSubmit called with:", values);
+    if (values.query.length < 3) return;
+
+    localStorage.setItem("query", values.query);
+    asyncSubmit(() =>
+      newsapi.getNewsCards(values.query).then((data) => {
+        console.log("Fetched news data:", data);
+        setNewsItems(data);
+      })
+    );
+  };
+
+  const handleRegisterSubmit = (values) => {
+    asyncSubmit(() =>
+      auth.register(values.email, values.password, values.username).then(() => {
+        handleLogin(values.email, values.password, () => {});
+      })
+    );
+  };
+
+  useEffect(() => {
+    newsapi.getNewsCards(query).then((data) => {
+      console.log("Fetched news data:", data);
+      setNewsItems({ ...data, articles: data.articles || [] });
+    });
+  }, [query]);
+
+  useEffect(() => {
+    const storedSavedArticles =
+      JSON.parse(localStorage.getItem("savedArticles")) || [];
+    console.log("Loaded savedArticles from localStorage:", storedSavedArticles);
+    setSavedArticles(storedSavedArticles);
+  }, []);
+
+  useEffect(() => {
+    const savedQuary = localStorage.getItem("query");
+    if (savedQuary) {
+      setQuery(savedQuary);
+    }
+  }, []);
+
+  return (
+    <CurrentUserContext.Provider value={{ currentUser, setCurrentUser }}>
+      <SavedArticlesContext.Provider
+        value={{ savedArticles, setSavedArticles }}
+      >
+        <div className="page">
+          <Header
+            handleSearchSubmit={handleSearchSubmit}
+            query={query}
+            setQuery={setQuery}
+            openLoginModal={openLoginModal}
+            handleLogOut={handleLogOut}
+          />
+
+          <div className="page-content">
+            <Routes>
+              <Route
+                path="/"
+                element={
+                  <>
+                    {query && (
+                      <Main
+                        newsData={newsData}
+                        newsItems={newsItems}
+                        handleNewsSaved={handleNewsSaved}
+                        isLoading={isLoading}
+                      />
+                    )}
+                  </>
+                }
+              />
+              <Route
+                path="/saveNews"
+                element={
+                  <ProtectedRoute isLoggedIn={isLoggedIn}>
+                    <SavedArticles
+                      savedArticles={savedArticles}
+                      handleRemoveArticle={handleRemoveArticle}
+                    />
+                  </ProtectedRoute>
+                }
+              />
+            </Routes>
+          </div>
+          {location.pathname === "/" && <About />}
+          <Footer />
+        </div>
+
+        {activeModal === "login" && (
+          <LoginModal
+            activeModal={activeModal}
+            closeActiveModal={closeActiveModal}
+            handleLogin={handleLogin}
+            setActiveModal={setActiveModal}
+          />
+        )}
+        {activeModal === "register" && (
+          <RegisterModal
+            activeModal={activeModal}
+            closeActiveModal={closeActiveModal}
+            handleRegisterSubmit={handleRegisterSubmit}
+            setActiveModal={setActiveModal}
+          />
+        )}
+        {activeModal === "registerSuccess" && (
+          <RegisterMessage
+            closeActiveModal={closeActiveModal}
+            setActiveModal={setActiveModal}
+          />
+        )}
+      </SavedArticlesContext.Provider>
+    </CurrentUserContext.Provider>
+  );
+}
+
+export default App;
