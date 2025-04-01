090129800ee582c4d0c8c7356a447747ae869c83
sun4205 <hanna4205@gmail.com>
2025-03-31 21:51:29 -0400

    practice project

--- review.js
+++ review.js
@@ -1,64 +1,17 @@
-const baseUrl = "http://localhost:3000";
 
-function checkResponse(res) {
-  return res.ok ? res.json() : Promise.reject(`Error:${res.status}`);
-}
-
-function request(url, options) {
-  return fetch(url, options).then(checkResponse);
-}
-
-function savedNews({ articleId, source, title, date, description, image }) {
-  console.log("Sending fetch request...");
-  console.log("ID being sent:", articleId); 
-  console.log("Data being sent:", {
-    id: articleId,  
-    source,
-    title,
-    date,
-    description,
-    image,
-  });
-
-  const token = localStorage.getItem("jwt");
-
-  return fetch(`${baseUrl}/saveNews`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      authorization: `Bearer ${token}`,
-    },
-    body: JSON.stringify({
-      id: articleId,  
-      source: source?.name,  
-      title,
-      date,
-      description,
-      image,
-    }),
-  }) 
-    .then((res) => {
-      console.log("Response status:", res.status);
-      return res.json();
-    })
-    .then((data) => {
-      console.log("Server response:", data);
-    })
-    .catch((error) => {
-      console.error("Error saving news:", error);
-    });
-}
-
-const removeNewsCardSved = (id, token) => {
-  return fetch(`${baseUrl}/saveNews/${id}`, {
+const removeNewsCardSved = (articleId, token) => {
+  
+  return fetch(`${baseUrl}/saveNews/${articleId}`, {
     method: "DELETE",
     headers: {
-      Authorization: `Bearer ${token}`,
       "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
     },
   })
-    .then((res) => res.json())
-    .catch((err) => console.log(err));
+  .then((res) => {
+    if (!res.ok) {
+      return Promise.reject(`Error: ${res.status}`);
+    }
+    return res.json();
+  });
 };
-
-export { checkResponse, savedNews, removeNewsCardSved };
