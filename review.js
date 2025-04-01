64c014b49246d1efcd8ab93096b479d01b3ea861
sun4205 <hanna4205@gmail.com>
2025-03-31 21:52:03 -0400

    project practice

--- review.js
+++ review.js
@@ -1,17 +1,58 @@
+import "./savedArticles.css";
+import NewsCard from "../NewsCard/NewsCard";
+import { useContext, useEffect } from "react";
+import CurrentUserContext from "../../contexts/CurrentUserContext";
 
-const removeNewsCardSved = (articleId, token) => {
+
+function SavedArticles({
+  savedArticles,
+  handleRemoveArticle,
+ setSavedArticles
+}) {
+  const { currentUser, setCurrentUser } = useContext(CurrentUserContext);
+ 
+  console.log("savedArticles:", savedArticles);
+
   
-  return fetch(`${baseUrl}/saveNews/${articleId}`, {
-    method: "DELETE",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${token}`,
-    },
-  })
-  .then((res) => {
-    if (!res.ok) {
-      return Promise.reject(`Error: ${res.status}`);
-    }
-    return res.json();
-  });
-};
+  const allKeywords = [...new Set( (savedArticles || []) 
+  .filter(item => item && item.keywords) 
+  .flatMap(item => item.keywords)
+)];
+
+const keywordsText = allKeywords.length > 2  
+  ? `${allKeywords.slice(0, 2).join(", ")} and ${allKeywords.length - 2} others`  
+  : allKeywords.join(", ");  
+
+
+  
+  if (!currentUser) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div className="savedArticles__container">
+      <p className="savedArticles__title">Saved articles</p>
+      <p className="savedArticles__numberSaved">
+        {currentUser.username}, you have {savedArticles.length} saved articles
+      </p>
+      <p className="savedArticles__by">
+        By keywords:{" "}
+        <span className="savedArticles__keywords">
+        {keywordsText}
+        </span>
+      </p>
+
+      <ul className="savedArticles__lists">
+        {savedArticles.map((item, index) => (
+          <NewsCard
+            key={index}
+            data={item}
+            handleNewsSaved={() => handleNewsSaved({ data: item })}
+            handleRemoveArticle={handleRemoveArticle}
+          />
+        ))}
+      </ul>
+    </div>
+  );
+}
+export default SavedArticles;
