daf6997f1e67b02a491b77b0572eb4478d880b14
sun4205 <hanna4205@gmail.com>
2025-04-02 19:52:26 -0400

    practice review

--- review.js
+++ review.js
@@ -1,79 +1,51 @@
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-const { JWT_SECRET } = require("../utils/config");
-const NotFoundError = require("../utils/errors/NotFoundError");
-const UnauthorizedError = require("../utils/errors/UnauthorizedError");
-const BadRequestError = require("../utils/errors/BadRequestError");
-const ConflictError = require("../utils/errors/ConflictError");
+import { useState } from "react";
 
-const savedArticles = [];
+export function useForm(initialValues) {
+  const [values, setValues] = useState(initialValues);
+  const [error, setError] = useState({});
 
-const getSavedArticles = (req, res, next) => {
-    const userId = req.user._id;
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setValues((prevValues) => ({
+      ...prevValues,
+      [name]: value,
+    }));
+  };
 
-    if(!userId) {
-        return next(new UnauthorizedError("please login!"))
-    }
-    const userArticles = savedArticles.filter((article) => article.userId === userId);
-    return res.status(200).json(userArticles);
+  
+const handleBlur = (e) => {
+  const { name, value } = e.target;
+  if (name === "email") {
+    validateEmail(value);
+  }
 };
 
-const savedArticle = (req,res,next) => {
-  console.log("Received data:", req.body);
-    const userId = req.user._id;
-    const { id: articleId, source, title, date, description, image,keywords  } = req.body; 
-
-    console.log(req.body)
-    if(!userId) {
-        return next(new UnauthorizedError("please login!"))
-    }
-    if (!articleId) {
-      return next(new BadRequestError("Article ID is required"));
-    }
-    const newArticle = {
-        id: articleId, 
-        userId, 
-        source:source?.name, 
-        title,
-        date,
-        description,
-        image,
-        keywords,
-      };
-      console.log("newArticle:", newArticle);
-    
-      savedArticles.push(newArticle);
-      console.log("Saved Article:", savedArticles);
-      return res.status(201).send(newArticle);
-    };
-    
-    
-    const deleteArticle = (req, res, next) => {
-      const articleId = req.params.id; 
-      const userId = req.user._id;
-    
-      if (!userId) {
-        return next(new UnauthorizedError("please login!"));
+  const validateEmail = (value) => {
+    if (value === "") {
+      setError((prevErrors) => ({
+        ...prevErrors,
+        email: "",
+      }));
+    } else if (!/\S+@\S+\.\S+/.test(value)) {
+      setError((prevErrors) => ({
+        ...prevErrors,
+        email: "Invalid Email address",
+      }));
+    } else {
+      const existingUser = users.find((user) => user.email === value);
+      if (existingUser) {
+        setError((prevErrors) => ({
+          ...prevErrors,
+          email: "This email is not available",
+        }));
+      } else {
+        setError((prevErrors) => ({
+          ...prevErrors,
+          email: "",
+        }));
       }
+    }
+  };
 
-      console.log("Saved Articles:", savedArticles);
-      console.log("Article ID:", articleId);
-      console.log("User ID:", userId);
-    
-      const articleIndex = savedArticles.findIndex((article) => article.id === articleId  && article.userId === userId);
-    
-      if (articleIndex === -1) {
-        console.log("Article not found:", articleId);
-        return next(new NotFoundError("News article not found."));
-      }
-
-      console.log("Article found:", savedArticles[articleIndex]);
-    
-      savedArticles.splice(articleIndex, 1); 
-      return res.status(200).json({ message: "news is deleted" });
-    };
-    
-    module.exports = { savedArticle, getSavedArticles, deleteArticle };
-    
-
-
+  return { values, error, handleBlur, handleChange, setValues };
+}
