4ec679f12d82f1f80a877e53ead61fa91a600347
sun4205 <hanna4205@gmail.com>
2025-04-02 20:44:54 -0400

    practice review

--- review.js
+++ review.js
@@ -1,51 +1,18 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-export function useForm(initialValues) {
-  const [values, setValues] = useState(initialValues);
-  const [error, setError] = useState({});
+  function useDebounce (value,delay=500) {
+    const [debouncedValue, setDebouncedValue] = useState(value);
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setValues((prevValues) => ({
-      ...prevValues,
-      [name]: value,
-    }));
-  };
-
-  
-const handleBlur = (e) => {
-  const { name, value } = e.target;
-  if (name === "email") {
-    validateEmail(value);
-  }
-};
-
-  const validateEmail = (value) => {
-    if (value === "") {
-      setError((prevErrors) => ({
-        ...prevErrors,
-        email: "",
-      }));
-    } else if (!/\S+@\S+\.\S+/.test(value)) {
-      setError((prevErrors) => ({
-        ...prevErrors,
-        email: "Invalid Email address",
-      }));
-    } else {
-      const existingUser = users.find((user) => user.email === value);
-      if (existingUser) {
-        setError((prevErrors) => ({
-          ...prevErrors,
-          email: "This email is not available",
-        }));
-      } else {
-        setError((prevErrors) => ({
-          ...prevErrors,
-          email: "",
-        }));
-      }
-    }
-  };
-
-  return { values, error, handleBlur, handleChange, setValues };
+    useEffect(() => {
+        const timerID = setTimeout(() => {
+          setDebouncedValue(value);
+        }, delay);
+    
+        return () => {
+          clearTimeout(timerID); 
+        };
+      }, [value, delay]);
+    return debouncedValue;
 }
+
+export default useDebounce;
